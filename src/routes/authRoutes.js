5c88366f1744bfb18a4bf8b8330126400abb3d94
Roger Generoso Masós <roger21gm@gmail.com>
2019-12-24 12:50:44 +0100

    Implemented secure signup and signin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -20,8 +20,26 @@ router.post('/signup', async (req, res) => {
     } catch (e) {
         res.status(422).send(e.message);
     }
+});
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
 
+    if(!email || !password)
+        return res.status(422).send({ error: 'Must provide email and password' });
+
+    const user = await User.findOne({ email });
+    if(!user){
+        return res.status(422).send({ error: 'Email not found' });
+    }
+
+    try{
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'SECRET_KEY');
+        res.send({token});
+    } catch (e) {
+        return res.status(422).send({ error: 'Invalid password or email' });
+    }
 
 });
 
