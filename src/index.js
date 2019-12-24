af7f06edc1bd39c758672faf1e3474efc677029e
Roger Generoso Masós <roger21gm@gmail.com>
2019-12-24 11:58:55 +0100

    New track-app. Iinitial commit of express mongo-db server.

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-rgdan.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo instance')
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo -> ', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
+
